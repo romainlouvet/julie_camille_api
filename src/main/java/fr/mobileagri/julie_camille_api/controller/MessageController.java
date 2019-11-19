58a5c9394e08b48ff2fcb4c28121316985762950
Romain LOUVET <romain.louvet@adevinta.com>
2019-11-19 13:42:10 +0100

    fix: set Request mapping

--- src/main/java/fr/mobileagri/julie_camille_api/controller/MessageController.java
+++ src/main/java/fr/mobileagri/julie_camille_api/controller/MessageController.java
@@ -1,13 +1,12 @@
 package fr.mobileagri.julie_camille_api.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
-  @RequestMapping(value = "/getDateAndTime")
+  @RequestMapping("/getDateAndTime")
   public String getDateAndTime() {
 
     //
